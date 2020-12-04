8067457ce4d167e91d56d18a590e230338a57873
Pepe <Pepe@DESKTOP-Q3M2R9O>
2020-12-04 15:13:14 -0700

    test

--- src/Prog4.java
+++ src/Prog4.java
@@ -1,4 +1,4 @@
-// @authors: David Dung, Graysen Meyers
+// @authors: David Dung, Graysen Meyers, Cullen Bates
 
 import java.io.*;
 import java.sql.*; // For access to the SQL interaction methods
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Prog4 {
 	public static void main(String[] args) {
 		// get user input
-
+		System.out.println("Testing new branch");
 		// insert
 
 		// delete
