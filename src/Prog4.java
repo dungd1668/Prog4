2ba24e52f7cf1adb339bf5c806c6f5c143a95443
David <David@DESKTOP-Q9V7MHC>
2020-12-04 14:11:44 -0700

    testing

--- src/Prog4.java
+++ src/Prog4.java
@@ -1,9 +1,18 @@
+// @authors: David Dung, 
+
 import java.io.*;
 import java.sql.*; // For access to the SQL interaction methods
 import java.util.Scanner;
 
 public class Prog4 {
 	public static void main(String[] args) {
+		// get user input
+		
+		// insert
+		
+		// delete
+		
+		// update
 		
 	}
 }
