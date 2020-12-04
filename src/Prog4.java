459616de937d74a02be23acc7cac7fdfa2b0a016
David <David@DESKTOP-Q9V7MHC>
2020-12-04 13:48:49 -0700

    created main file

---
+++ src/Prog4.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.sql.*; // For access to the SQL interaction methods
+import java.util.Scanner;
+
+public class Prog4 {
+	public static void main(String[] args) {
+		
+	}
+}
